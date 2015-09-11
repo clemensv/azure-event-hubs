bdff3beb2e23a6098330f6e830416084c64aa089
Clemens Vasters <clemens@vasters.com>
2015-09-11 11:10:09 +0200

    Update compiler_options.cmake

--- cmake/compiler_options.cmake
+++ cmake/compiler_options.cmake
@@ -1,5 +1,11 @@
-IF (NOT WIN32)
+if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
   set(CMAKE_C_FLAGS "-std=c99 -pedantic -Wall -w")
   set(CMAKE_CXX_FLAGS "-std=c++11 -stdlib=libc++ -pedantic -Wall -w")
-ENDIF()
-
+elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+  set(CMAKE_C_FLAGS "-std=c99 -pedantic -Wall -w")
+  set(CMAKE_CXX_FLAGS "-std=c++11 -pedantic -Wall -w")
+elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
+  # using Intel C++
+elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
+  # using Visual Studio C++
+endif()
